cd84e6dff293635d1b9cbdff89bfed1b8deb69c3
MSuzu190331 <m.suzuki.0u0@gmail.com>
2020-10-20 21:54:04 +0900

    投稿、使用グッズ、アイテム、ブランドテーブル追加 カラム追加による微修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,6 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:contents)
+      params.require(:post).permit(:contents, :image)
     end
 end
